3fa07c64cf5783720bfcfb8aa4e09c40f48c7245
Reza <rezafathan123254@gmai.com>
2022-04-30 21:18:51 +0700

    Initial Commit

---
+++ NANO 1/TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableViewCell.swift
+//  NANO 1
+//
+//  Created by Reza Mac on 27/04/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var contentLabelTwo: UILabel!
+    @IBOutlet weak var labelStatus: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
